83dceccbadeb28b3c55bca6cc0e47a9a2bba31a9
lazarogmcassiano <139990351+lazarogmcassiano@users.noreply.github.com>
2026-08-25 21:55:55 -0300

    chore: add clangd for C development

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("codecompanion").setup({
+        adapters = {
+          ollama = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              env = {
+                url = "http://127.0.0.1:11434",
+              },
+              schema = {
+                model = {
+                  default = "qwen2.5-coder:7b",
+                },
+              },
+            })
+          end,
+        },
+
+        strategies = {
+          chat = {
+            adapter = "ollama",
+          },
+
+          inline = {
+            adapter = "ollama",
+          },
+
+          agent = {
+            adapter = "ollama",
+          },
+        },
+      })
+    end,
+  },
+}
